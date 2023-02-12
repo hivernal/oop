1e014ab3502682421ecfaff807fc3fd4171732b5
hivernal <geryaevna@mail.ru>
2023-02-12 20:57:13 +0700

    add container in main function, filled him from file and output in file

--- container_in.cpp
+++ container_in.cpp
@@ -2,7 +2,7 @@
 
 namespace encryption_methods {
   void Container::in(std::ifstream& in_file) {
-    for (size_t i; in_file.peek() != EOF && len < MAX_SIZE; ++i) {
+    for (size_t i = 0; in_file.peek() != EOF && len < MAX_SIZE; ++i) {
       if ((array[i] = Encryption::in(in_file)) != 0)
         ++len;
     }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
+#include "container.h"
+
+#include <fstream>
 #include <iostream>
 
-int main (int argc, char *argv[]) {
-  
-  return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cout << "incorrect command line!\
+                  Waited: command in_file out_file" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  std::ifstream in_file(argv[1]);
+  std::ofstream out_file(argv[2]);
+  std::cout << "Start" << std::endl;
+  encryption_methods::Container cont;
+  cont.in(in_file);
+  out_file << "Filled container. " << std::endl;
+  cont.out(out_file);
+  cont.clear();
+  out_file << "Empty container. " << std::endl;
+  cont.out(out_file);
+  std::cout << "Stop" << std::endl;
+  return EXIT_SUCCESS;
 }
